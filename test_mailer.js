f5e28b7899c8eaaa8cb501f622486312044af6c8
Romain Laneuville <romain.laneuville@hotmail.fr>
2018-05-09 10:19:46 +0200

    Pas mon email dans les examples pls

--- test_mailer.js
+++ test_mailer.js
@@ -1,4 +1,4 @@
 var mailer = require('./index.js');
 
 var transporter = mailer.initMailer('gmail', 'eco.transport.campus@gmail.com', 'APP52018');
-mailer.sendMail(transporter, 'eco.transport.campus@gmail.com', 'romain.laneuville@u-psud.fr', 'Random subject', 'Hola chico');
+mailer.sendMail(transporter, 'eco.transport.campus@gmail.com', 'prenom.nom@u-psud.fr', 'Random subject', 'Hola chico');
